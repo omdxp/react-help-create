2bc302666688cfaa39e57d750fa429b1ce033de0
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-02-10 14:28:08 +0100

    combining screens and components in new project structure

--- bin/combine/components/combine-components.js
+++ bin/combine/components/combine-components.js
@@ -5,7 +5,7 @@ const fs = require("file-system");
  * @description this function is used to combine components in a specific folder.
  * @param {Array} components - array of components to be combined.
  * @param {string} folder - the name or path of folder that combines the components.
- * @version 1.0.0
+ * @version 2.0.0
  * @author [Omar Belghaouti](https://github.com/Omar-Belghaouti)
  */
 exports.combineComponents = (components, folder) => {
@@ -13,35 +13,33 @@ exports.combineComponents = (components, folder) => {
     console.log("Please provide at least 2 components");
     return;
   }
-  let files = [];
+  const path = "src/components/";
+  const _path = `${path}${folder}/`;
+  let folders = [];
   components.forEach((component) => {
-    fs.readdirSync(`src/components/`)
-      .filter((file) => file.startsWith(component.toLowerCase()))
-      .forEach((file) => {
-        files.push(file);
-      });
+    fs.readdirSync(path)
+      .filter((f) => f === component)
+      .forEach((f) => folders.push(f));
   });
-  if (files.length < components.length) {
-    console.log("Check if these components do exist");
+  if (folders.length < components.length) {
+    console.log("Check if all of these components exist");
     return;
   }
-  const path = `src/components/${folder}`;
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
+  if (!fs.existsSync(_path)) {
+    fs.mkdirSync(_path);
   } else {
-    console.log(`${path} already exist`);
+    console.log(`${_path} already exist`);
     console.log("Writing new files...");
   }
-  files.forEach((file) => {
-    const path = `src/components/${folder}/${file}`;
-    if (fs.existsSync(path)) {
-      console.log(`${path} already exist`);
+  folders.forEach((f) => {
+    if (fs.existsSync(`${_path}${f}/`)) {
+      console.log(`${_path}${f}/ already exist`);
     } else {
-      fs.rename(`src/components/${file}`, path, (err) => {
+      fs.rename(`${path}${f}/`, `${_path}${f}`, (err) => {
         if (err) {
-          console.log(`Cannot move ${file} component`);
+          console.log(`Cannot move ${f} component`);
         } else {
-          console.log(`${file} component moved to src/components/${folder}/`);
+          console.log(`${f} component moved to ${_path}`);
         }
       });
     }
